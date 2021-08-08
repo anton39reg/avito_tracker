34709e455bbc83bfc30bfe15a02c7be22bd65760
Pilkevich Anton <pilkevich.aa@phystech.edu>
2021-08-08 14:43:59 +0300

    fix paths

--- server/configure_and_run.py
+++ server/configure_and_run.py
@@ -4,8 +4,8 @@ from pyngrok import ngrok
 def main() -> None:
     """Start the bot."""
 
-    http_tunnel = ngrok.connect(1234, bind_tls=True)
-    with subprocess.Popen(['python3', 'flask_server.py', http_tunnel.public_url]) as proc:
+    http_tunnel = ngrok.connect(2781, bind_tls=True)
+    with subprocess.Popen(['python3', 'server/flask_server.py', http_tunnel.public_url]) as proc:
         proc.wait()
 
 if __name__ == '__main__':
